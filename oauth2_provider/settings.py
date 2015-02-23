d58f077b42849594770b3704923cc1460d9a8f71
baffolobill <baffolobill@yandex.ru>
2015-02-24 01:05:53 +0300

    APPLICATION_MODEL now requires full path for Document

--- oauth2_provider/settings.py
+++ oauth2_provider/settings.py
@@ -39,7 +39,7 @@ DEFAULTS = {
     'WRITE_SCOPE': 'write',
     'AUTHORIZATION_CODE_EXPIRE_SECONDS': 60,
     'ACCESS_TOKEN_EXPIRE_SECONDS': 36000,
-    'APPLICATION_MODEL': getattr(settings, 'OAUTH2_PROVIDER_APPLICATION_MODEL', 'oauth2_provider.Application'),
+    'APPLICATION_MODEL': getattr(settings, 'OAUTH2_PROVIDER_APPLICATION_MODEL', 'oauth2_provider.models.Application'),
     'REQUEST_APPROVAL_PROMPT': 'force',
     'ALLOWED_REDIRECT_URI_SCHEMES': ['http', 'https'],
 
